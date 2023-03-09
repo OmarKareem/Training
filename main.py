38f028a2c97f34f82511ac5d04a7f3416309d48c
Omar Kareem <omar.the.sentinel@gmail.com>
2023-03-09 19:36:40 +0200

    d

---
+++ main.py
@@ -0,0 +1,17 @@
+age = 99
+
+
+if age < 20 :
+    for x in range (101):
+        # i want to print odd number
+
+        if x % 2 > 0:
+            print(x)
+else:
+
+
+        # i want to print even numbers
+        # when i divided variable x by 2 the reminder == 0
+    for x in range (51):
+        if x % 2 == 0:
+            print(x)
